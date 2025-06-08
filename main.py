b6fc4d0834fcefd5475744f68c3c16903b521ac5
nnatuanyafrankoguguo <nnatuanyafrank@gmail.com>
2025-06-08 19:26:59 +0100

    ok

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,  APIRouter
 from fastapi.middleware.cors import CORSMiddleware
 from oauth_router import router as auth_router
 from calendar_router import router as prompt_router
@@ -7,6 +7,7 @@ from sqlalchemy.exc import OperationalError
 from model import Base
 
 app = FastAPI()
+router = APIRouter()
 
 app.add_middleware(
     CORSMiddleware,
@@ -16,9 +17,18 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+@router.get("/")
+async def root():
+    return {"message": "Welcome to the Calendar API!"}
+
+app.include_router(router)
+
 # Mount routers
 app.include_router(auth_router, prefix="/oauth", tags=["OAuth"])
 app.include_router(prompt_router, prefix="/prompt", tags=["Prompt"])
+
+
+
 
 try:
     with engine.connect() as connection:
